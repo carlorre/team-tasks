74b97ff721c45c30cc5b19a2609c1f8210c68e26
Carl Orre <35249653+carlorre@users.noreply.github.com>
2020-11-24 08:56:54 +0100

    Update useDetectOutsideClick.ts

--- src/hooks/useDetectOutsideClick.ts
+++ src/hooks/useDetectOutsideClick.ts
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 
 export const useDetectOutsideClick = (el:any, initialState:any) => {
-  console.log(initialState)
   const [isActive, setIsActive] = useState(initialState);
 
   useEffect(() => {
